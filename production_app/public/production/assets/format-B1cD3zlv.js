ba72aa613f7c385b8d0ca0d769eb279c9fe06356
Muhammad Yusuf Tri Daryanto <114971841+Muhayustrid@users.noreply.github.com>
2026-09-11 09:16:32 +0700

    build: committed SPA build output (sourcemaps off)

--- production_app/public/production/assets/format-B1cD3zlv.js
+++ production_app/public/production/assets/format-B1cD3zlv.js
@@ -1,4 +1,4 @@
-import{c}from"./index-_mfzRwAb.js";/**
+import{c}from"./index-BHz2eTSi.js";/**
  * @license lucide-vue-next v0.469.0 - ISC
  *
  * This source code is licensed under the ISC license.
